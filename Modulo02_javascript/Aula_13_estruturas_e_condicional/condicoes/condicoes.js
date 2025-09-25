50a6649cd3b7d3a910866e85ead0d321ecdf884c
giovannialmeida0907 <almeida110609@gmail.com>
2025-09-25 09:15:01 -0300

    .

---
+++ Modulo02_javascript/Aula_13_estruturas_e_condicional/condicoes/condicoes.js
@@ -0,0 +1,17 @@
+let nota1 = 10
+let nota2 = 10
+
+let media = (nota1 + nota2)/2
+
+if (media >= 7) {
+    console.log("Aprovado")
+} 
+
+else if(media >=5 ){
+    console.log("Recuperação")
+}
+
+else {
+    console.log("Reprovado")
+}
+
